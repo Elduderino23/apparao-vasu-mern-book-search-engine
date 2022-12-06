04e7085f00d10a705af0c48ede6a3824db5ec7ee
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-12-05 20:57:51 -0800

    starter code

---
+++ client/build/precache-manifest.93c757f67d638c5c8dcccdcbeb764ca0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b2fc581c516a16887000c45b75a969",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c26404c69d86e723b9c",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "94fc2d8ae8385d22ac7a",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "7c26404c69d86e723b9c",
+    "url": "/static/js/2.1797b3e0.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/static/js/2.1797b3e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94fc2d8ae8385d22ac7a",
+    "url": "/static/js/main.5b3fd172.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.5b3fd172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
