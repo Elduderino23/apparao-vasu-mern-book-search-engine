602c7487a6198bb4ccadfccd5bb3e224a2519648
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-12-07 21:54:13 -0800

    askBCS Troubleshoot 2

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -3,44 +3,19 @@ import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap
 
 import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
-import { useQuery } from '@apollo/client';
 import { removeBookId } from '../utils/localStorage';
 import { GET_ME } from '../utils/queries';
 import {useMutation} from '@apollo/client'
+import { useQuery } from '@apollo/client';
 import { REMOVE_BOOK } from '../utils/mutations';
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  
   // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
-  // const [removeBook] = useMutation(REMOVE_BOOK)
-  const { loading, data } = useQuery(GET_ME, {
-    
-    variables: { bookId: bookId },
-  });
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+  
+  const [removeBook] = useMutation(REMOVE_BOOK)
+  const { loading, data } = useQuery(GET_ME)
+  const userData = data?.me || {};
 
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -57,8 +32,7 @@ const SavedBooks = () => {
         throw new Error('something went wrong!');
       }
 
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
+      
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
@@ -67,7 +41,7 @@ const SavedBooks = () => {
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
